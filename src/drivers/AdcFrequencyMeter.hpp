951e306e44cb4d9c96ad8bb52c8e32fba5b4c117
Danilo <129679035+X105GHM@users.noreply.github.com>
2026-02-23 07:39:43 +0100

    Delete src directory

--- src/drivers/AdcFrequencyMeter.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-namespace drivers::adc_freq
-{
-    void init();
-
-    [[nodiscard]] bool readFrequencyHz(uint16_t &frequencyHz);
-} 
--- src/drivers/PwmOutput.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-namespace drivers::pwm
-{
-    void init();
-    void setDuty(uint8_t duty);
-}
